19b7b523c26bdde6dc546e2a3bc64c23b622947f
Bharathi <rbharathi7995@gmail.com>
2026-05-19 20:42:31 +0530

    get various citites weather report by using geocoding-api to get latitudes and longitudes then change the weather api

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,17 +1,30 @@
- 
+ let latitude='';
+ let longitude='';
+ let name='';
  document.querySelector('.js-search-button').addEventListener('click',()=>{
     const area=document.querySelector('.js-area-search').value;
    
     document.querySelector('.js-city-name').innerHTML=area;
-
-  async function renderAreaInfo() {
+ async function renderAreaInfo() {
     
   
- 
+    async function areaName(){
+        try{
+        const reaction= await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${area}&count=1`);
+        const areaInfo=await reaction.json();
+        
+        name=areaInfo.results[0].name;
+        latitude=areaInfo.results[0].latitude
+        longitude=areaInfo.results[0].longitude;
+        }
+        catch{
+            console.log("Location not found");
+        }
+    }
    
     async function weatherInfo(){
-       
-        const response= await fetch('https://api.open-meteo.com/v1/forecast?latitude=17.08&longitude=77.09&current=temperature_2m,wind_speed_10m,relative_humidity_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=auto')
+         await areaName();
+        const response= await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=auto`)
         const info=await response.json();
         
         document.querySelector('.js-time').innerHTML=info.current.time;
