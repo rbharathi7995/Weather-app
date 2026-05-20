11cd0a4e6aedfbb802a342ad8ba2e5e344c68afb
Bharathi <rbharathi7995@gmail.com>
2026-05-20 21:02:28 +0530

    handles invalid city by using if else stmts

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,52 @@
-
+let latitude='';
+let longitude='';
+let name='';
  
  document.querySelector('.js-search-button').addEventListener('click',()=>{
     const area=document.querySelector('.js-area-search').value;
-    localStorage.setItem('city',area);
+  /*    if(area === ''){
+      document.querySelector('.js-search-valid-msg').innerHTML="Please enter valid city name";
+      return;
+    }
+*/
+    async function renderAreaInfo() {
+       async function areaName(){
+        
+        const reaction= await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${area}&count=1`);
+        const areaInfo=await reaction.json();
+       
+        if(!areaInfo.results || areaInfo.results.length === 0){
+         return false;
+        }
+        else{
+        name=areaInfo.results[0].name;
+        latitude=areaInfo.results[0].latitude
+        longitude=areaInfo.results[0].longitude;
 
-    window.location.href='weather.html';
+        return true;
+        }
+        
+    }
+    const validCity=await areaName();
+  
+   if(validCity === false){
+    document.querySelector('.js-search-valid-msg').innerHTML="Invalid city name"
+
+   setTimeout(()=>{
+    document.querySelector('.js-search-valid-msg').innerHTML=""
+
+    },3000);
+   
+   }
+
+   else{
+  localStorage.setItem('city',area);
+  window.location.href='weather.html';
+   }
+  
+  }
+    renderAreaInfo();
+
  })
+ 
  
--- scripts/weather.js
+++ scripts/weather.js
@@ -8,24 +8,40 @@
     
   
     async function areaName(){
-        try{
+        
         const reaction= await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${area}&count=1`);
         const areaInfo=await reaction.json();
         
         name=areaInfo.results[0].name;
         latitude=areaInfo.results[0].latitude
         longitude=areaInfo.results[0].longitude;
-        }
-        catch{
-            console.log("Location not found");
-        }
+        
+        
+      
     }
    
     async function weatherInfo(){
          await areaName();
-        const response= await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=auto`)
+        const response= await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m,weather_code&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=auto`)
         const info=await response.json();
+        const weatherCode=info.current.weather_code
         
+        if(weatherCode === 0){
+            document.querySelector('.js-weather-condition').innerHTML='☀️Sunny'
+        }
+        else if(weatherCode >=1 && weatherCode <=3 ){
+            document.querySelector('.js-weather-condition').innerHTML='☁️Cloudy'
+        }
+        else if(weatherCode >=51 && weatherCode <=65 ){
+            document.querySelector('.js-weather-condition').innerHTML='🌧️Rainy'
+        }
+        else if(weatherCode >=95 ){
+            document.querySelector('.js-weather-condition').innerHTML='⛈️Thunderstorm'
+        }
+
+
+
+
         document.querySelector('.js-time').innerHTML=info.current.time;
         document.querySelector('.js-temperature').innerHTML=info.current.temperature_2m+"&deg;C";
         document.querySelector('.js-wind-speed').innerHTML=info.current.wind_speed_10m+" km/h";
